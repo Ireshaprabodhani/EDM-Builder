e0bc19a47e07b02e635d4449008e705305ac1c88
Ireshaprabodhani <84035631+Ireshaprabodhani@users.noreply.github.com>
2024-11-04 13:46:19 +0530

    fix errors

--- src/components/EDMBuilder/components/ColumnComponent.jsx
+++ src/components/EDMBuilder/components/ColumnComponent.jsx
@@ -37,7 +37,7 @@ const ColumnComponent = ({
       </div>
 
       {showStyleEditor && (
-        < 
+        <StyleEditor 
           styles={styles}
           onUpdate={(newStyles) => onUpdateStyles(tableId, rowId, column.id, null, newStyles)}
         />
